e6af99dc3d3122a4d88d90b9f01a64aa7b1b70c8
mitibirru <357miteshbirru@gmail.com>
2022-07-28 18:15:21 +0530

    added profile

--- app.js
+++ app.js
@@ -112,9 +112,9 @@ app.get("/about", function (req, res) {
 
 let port = process.env.PORT;
 if (port == null || port == "") {
-  port = 8000;
+  port = 3000;
 }
 
 app.listen(port, function () {
-  console.log("Server has started on the port 3000");
+  console.log("Server has started on the port " + port);
 });
